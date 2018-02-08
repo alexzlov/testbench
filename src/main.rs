5f348074002d95a7664438dd0a635fe6519350e3
man213 <zlov.ehl@gmail.com>
2018-02-08 22:29:53 +0300

    Finish

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,28 @@ extern crate num;
 extern crate minifb;
 
 use minifb::{Key, WindowOptions, Window};
-
 use num::Complex;
-use std::str::FromStr;
 
-const WIDTH:  usize = 640;
-const HEIGHT: usize = 360;
 
-#[allow(dead_code)]
-fn complex_square_add_loop(c: Complex<f64>, limit: u32) -> Option<u32> {
-    let mut z = Complex { re: 0.0, im: 0.0 };
-    for i in 0..limit {
+const WIDTH:  usize = 1024;
+const HEIGHT: usize = 768;
+
+fn pixel_to_point(bounds:      (usize, usize),
+                  pixel:       (usize, usize),
+                  upper_left:  Complex<f64>,
+                  lower_right: Complex<f64>) -> Complex<f64> {
+
+    let (width, height) = (lower_right.re - upper_left.re,
+                           upper_left.im  - lower_right.im);
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width  / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}
+
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
+    let mut z = Complex {re: 0.0, im: 0.0};
+    for i in 0 .. limit {
         z = z * z + c;
         if z.norm_sqr() > 4.0 {
             return Some(i);
@@ -21,27 +32,51 @@ fn complex_square_add_loop(c: Complex<f64>, limit: u32) -> Option<u32> {
     None
 }
 
-fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
-    match s.find(separator) {
-        None => None,
-        Some(index) => {
-            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
-                (Ok(l), Ok(r)) => Some((l, r)),
-                _              => None
-            }
+fn render(pixels:      &mut [u32],
+          bounds:      (usize, usize),
+          upper_left:  Complex<f64>,
+          lower_right: Complex<f64>) {
+
+    assert!(pixels.len() == bounds.0 * bounds.1);
+    for row in 0 .. bounds.1 {
+        for column in 0 .. bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None        => 0,
+                Some(count) => (255 - count) << 16
+            };
         }
     }
 }
 
 fn main() {
-    let mut buffer: Vec<u32> = vec![0; 640 * 360];
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
     let mut window = Window::new(
         "Sample RGBA32 buffer", WIDTH, HEIGHT, WindowOptions::default()
     ).unwrap_or_else(|e| { panic!("{}", e); });
+    let mut upper_left  = Complex {re: -1.20, im: 0.35};
+    let mut lower_right = Complex {re: -1.0,  im: 0.20};
+    render(buffer.as_mut_slice(), (WIDTH, HEIGHT), upper_left, lower_right);
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        for i in buffer.iter_mut() {
-            *i = 127 << 16; // 16 - R, 0 - B, 8 - G
-            *i = 126;
+        if window.is_key_down(Key::Left) {
+            upper_left.re  -= 0.01;
+            lower_right.re -= 0.01;
+            render(buffer.as_mut_slice(), (WIDTH, HEIGHT), upper_left, lower_right);
+        }
+        if window.is_key_down(Key::Right) {
+            upper_left.re  += 0.01;
+            lower_right.re += 0.01;
+            render(buffer.as_mut_slice(), (WIDTH, HEIGHT), upper_left, lower_right);
+        }
+        if window.is_key_down(Key::Up) {
+            upper_left.im  += 0.01;
+            lower_right.im += 0.01;
+            render(buffer.as_mut_slice(), (WIDTH, HEIGHT), upper_left, lower_right);
+        }
+        if window.is_key_down(Key::Down) {
+            upper_left.im  -= 0.01;
+            lower_right.im -= 0.01;
+            render(buffer.as_mut_slice(), (WIDTH, HEIGHT), upper_left, lower_right);
         }
         window.update_with_buffer(&buffer).unwrap();
     }
